7b3b57ead41cb93492a99665a68b51ce15de492d
Carlos <carlosbandeiraufpr@gmail.com>
2026-02-12 22:07:27 -0300

    changes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,3 +1,5 @@
+// Cliente Supabase configurado
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
