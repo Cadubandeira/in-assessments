d0ab53dec4024a35caf688515f56c3934fd8aa00
Carlos <carlosbandeiraufpr@gmail.com>
2026-02-27 21:43:55 -0300

    changed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/in-assessments/',
+  base: '/',
   server: {
     port: 5173,
     strictPort: false, // Se 5173 estiver em uso, tenta próxima porta disponível
